705e40117a017f2ee869e899d310e83c5025fd6e
Sahreen Haider <sahreenhaider@gmail.com>
2024-07-09 10:50:01 +0530

    changed files

--- diffusion.py
+++ diffusion.py
@@ -1,22 +1,21 @@
+# importing the neccessary libraries
 import os
 import requests
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
+# loading the environment file
 load_dotenv()
 
 
-
+# getting the specific env variable
 access_token = os.getenv("Access_Token")
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
 headers = {"Authorization": f"Bearer {access_token}"}
-# print(access_token)
 
+
+# function for sending the query to the model and returning the reponse
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
 
-# print(image_bytes)
-
-# plt.imshow(image)
-# plt.show()
--- webapp.py
+++ webapp.py
@@ -1,27 +1,28 @@
+# importing necessary libs and modules
 import streamlit as st
+import io
+from PIL import Image
 from diffusion import *
 
-
+# setting the title of web app
 st.title("Image Generator")
 
-
+# inserting the query here
 ingested_query = st.text_input("Please enter your query here: ")
+
 
 if ingested_query:
     image_bytes = query({
         "inputs": ingested_query,
     })
     # You can access the image with PIL.Image for example
-    import io
-    from PIL import Image
-    
+
     image = Image.open(io.BytesIO(image_bytes))
     
+    # saving the generated image to "Data" directory
     image.save("Data/generated_image.jpeg", format='jpeg')
    
-    # image.save("generated_image.jpg", format='jpg')
 
-
-
+    # displaying the image and downloading it using "st.download_button()"
     st.image(image)
     st.download_button(label="Download", data=image_bytes, file_name="generated_image_download.jpg")
